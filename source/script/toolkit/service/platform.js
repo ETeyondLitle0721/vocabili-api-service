fa5f0db82cf3dba1e3207450efa7bab6d5e321e3
ETeyondLitle0721 <eteyondlitle@gmail.com>
2025-08-27 12:40:18 +0800

    清除platform.js文件中的无用代码

--- source/script/toolkit/service/platform.js
+++ source/script/toolkit/service/platform.js
@@ -16,33 +16,5 @@ const instance = new SQLite3(filepath, {
 
 const operator = new DatabaseOperator(instance);
 
-/**
- * 构造在冲突时修改构建结果的审查函数
- * 
- * @typedef {{ sentence: string }[]} ReviewerList
- * 
- * @param {string} conflict 描述语句
- * @returns {((list: ReviewerList) => ReviewerList)} 审查函数
- */
-function on_conflict(conflict) {
-    const _reviewer = (result) => {
-        return result.map((current) => {
-            const { sentence } = current;
-
-            current.sentence = sentence.replace(
-                "RETURNING", conflict + " RETURNING"
-            );
-
-            return current;
-        });
-    };
-
-    return _reviewer;
-}
-
-const extract = (field) => {
-    return (item) => item[field];
-};
-
 const MIC = 8192; // 单次插入标识符最大数量
 
