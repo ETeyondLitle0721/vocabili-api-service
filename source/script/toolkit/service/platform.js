88916269105def60f1faae3b449cd020b6e3a90f
ETeyondLitle0721 <eteyondlitle@gmail.com>
2025-08-27 21:55:21 +0800

    合并platform的upsert_video与upsert_uploader函数的冗余部分到upsert_item

--- source/script/toolkit/service/platform.js
+++ source/script/toolkit/service/platform.js
@@ -19,6 +19,237 @@ const operator = new DatabaseOperator(instance);
 const MIC = 8192; // 单次插入标识符最大数量
 
 /**
+ * 修改对象的字段名称和字段数值
+ * 
+ * @typedef {Object} Handler
+ * @property {() => void} discard 删除字段
+ * @property {(name: string) => void} rename 修改字段名称
+ * 
+ * @callback Replacer 字段数值替换器
+ * @param {string} key 字段名称
+ * @param {any} value 字段数值
+ * @param {Handler} handler 字段操作控制器
+ * 
+ * @template {Object} U
+ * @param {U} object 需要修改的对象
+ * @param {Replacer} replacer 字段数值替换器
+ * @returns {U} 修改过后的对象
+ */
+function modify(object, replacer) {
+    object = structuredClone(object);
+
+    const entries = Object.entries(object);
+
+    for (let [ key, value ] of entries) {
+        const result = replacer(key, value, {
+            "rename": (name) => key = name,
+            "discard": () => delete object[key]
+        });
+
+        if (result !== undefined) {
+            value = result;
+        }
+
+        object[key] = value;
+    }
+
+    return object;
+}
+
+/**
+ * 在数据库中数据库更新或者插入条目
+ * 
+ * @typedef {Object} Response
+ * @property {number} id 视频内部标识符
+ * @property {number} exterior 视频外部标识符
+ * @property {number} update_count 更新次数
+ * 
+ * @callback Mapper 数据表表单字段数值映射器
+ * @param {object[]} result 在数据库中查找外部识别码找的结果
+ * @param {object[]} input 需要更新或者插入的条目
+ * @returns {Object<string, Object<string, any>>} 数据表表单字段数值映射表
+ * 
+ * @typedef {Object} Options
+ * @property {string} table_name 操作的目标数据表名称
+ * @property {Mapper} mapper 数据表表单字段数值映射器
+ * 
+ * @param {(object|object[])} items 需要更新或者插入的条目
+ * @param {Options} options 更新或者插入的时候的配置 
+ * @param {Date} [instance=new Date()] 执行该操作时的 Date 实例化对象
+ * @returns {Response[]} 更新或者插入的视频信息
+ */
+function upsert_item(items, options, instance = new Date()) {
+    if (!Array.isArray(items)) {
+        items = [ items ];
+    }
+
+    if (items.length > MIC) {
+        const responses = [];
+
+        const groups = split_group(items, MIC);
+
+        for (let index = 0; index < groups.length; index++) {
+            const current = groups[index];
+
+            const result = upsert_item(
+                current, options, instance
+            );
+
+            responses.push(result);
+        }
+
+        return responses.flat(1);
+    }
+
+    const record = operator.record();
+
+    const eid_list = items.map(
+        (item) => item.exterior
+    );
+
+    const condition = {
+        "column": "exterior",
+        "restrict": {
+            "include": eid_list
+        }
+    };
+
+    const { table_name } = options;
+
+    const response = {}, field_list = [
+        "id", "exterior", "update_count"
+    ];
+
+    response.select = record.select(
+        table_name, condition, {
+            "select": field_list
+        }
+    )[0].flat(2);
+
+    /**
+     * @description 已存在的项目外部标识符
+     * 
+     * @type {number[]}
+     */
+    const exist_eid = response.select.map(
+        (record) => record.exterior
+    );
+
+    const current_datetime = instance.toISOString();
+
+    const mapping = options.mapper(
+        response.select, items
+    );
+
+    mapping.update_count = Object.fromEntries(
+        response.select.map((record) => [
+            record.exterior, record.update_count + 1
+        ])
+    );
+
+    if (exist_eid.length > 0) {
+        const condition = {
+            "column": "exterior",
+            "restrict": {
+                "include": exist_eid
+            }
+        };
+
+        const build = operator.statement().build;
+
+        const generator = (field) => (options) => {
+            return build.case.abstract.single(
+                "exterior", mapping[field], options
+            );
+        };
+
+        const options = {
+            "action": "execute",
+            "return_field": field_list
+        };
+
+        response.update = record.update(
+            table_name, condition, {
+                ...modify(mapping, (key) => {
+                    return generator(key);
+                }),
+                "updated_at": current_datetime,
+                "update_count": generator("update_count"),
+            }, options
+        )[0].flat(2);
+    }
+
+    const item_list = items.filter((item) => {
+        const exterior = item.exterior;
+
+        return !exist_eid.includes(exterior);
+    });
+
+    const dataset = [];
+
+    for (let index = 0; index < item_list.length; index++) {
+        const item = item_list[index];
+
+        dataset.push({
+            ...item, "update_count": 1,
+            "updated_at": current_datetime,
+            "recorded_at": current_datetime
+        });
+    }
+
+    if (dataset.length > 0) {
+        const options = {
+            "mode": "batch",
+            "action": "execute",
+            "return_field": field_list
+        };
+
+        response.insert = record.insert(
+            table_name, dataset, options
+        )[0].flat(2);
+    }
+
+    response.insert ??= [];
+    response.update ??= [];
+
+    const { update, insert } = response;
+
+    return [ update, insert ].flat(1);
+}
+
+/**
+ * 生成数据表表单字段数值映射器
+ * 
+ * @param {Record<(string|number), Object>} mapping 生成映射器需要使用的映射表
+ * @param {string[]} fields 需要映射的表单字段名称列表
+ * @returns {Mapper} 生成的映射器
+ */
+function generate_mapper(mapping, fields) {
+    return (records) => {
+        const results = {};
+
+        records.forEach((record) => {
+            const id = record.exterior;
+            const target = mapping[id];
+
+            const entries = Object.entries(target);
+
+            for (const [ key, value ] of entries) {
+                if (!fields.includes(key)) {
+                    continue;
+                }
+
+                results[key] ??= {};
+
+                results[key][id] = value;
+            }
+        });
+
+        return results;
+    };
+}
+
+/**
  * 更新或者插入上传者信息到数据库中
  * 
  * @typedef {Object} Uploader
@@ -36,144 +267,35 @@ const MIC = 8192; // 单次插入标识符最大数量
  * @returns {Response[]} 更新或者插入的上传者信息
  */
 function upsert_uploader(uploaders, instance = new Date()) {
-    if (!Array.isArray(uploaders)) {
-        uploaders = [ uploaders ];
+    const dataset = [], mapping = {};
+
+    for (let index = 0; index < uploaders.length; index++) {
+        const { id, name, avatar } = uploaders[index];
+
+        const data = {
+            "avatar": avatar,
+            "exterior": id,
+            "username": name,
+        };
+
+        dataset.push(data);
+        mapping[id] = data;
     }
 
-    if (uploaders.length > MIC) {
-        const responses = [];
-
-        const groups = split_group(uploaders, MIC);
-
-        for (let index = 0; index < groups.length; index++) {
-            const result = upsert_uploader(
-                groups[index], instance
-            );
-
-            responses.push(result);
-        }
-
-        return responses.flat(1);
-    }
-
-    const record = operator.record();
-
-    const condition = {};
-    
-    const eid_list = uploaders.map(
-        (uploader) => uploader.id
-    );
-
-    condition.select = {
-        "column": "exterior",
-        "restrict": {
-            "include": eid_list
-        }
-    };
-
-    const response = {}, field_list = [
-        "id", "exterior", "update_count"
+    const field_list = [
+        "avatar", "username"
     ];
 
-    response.select = record.select(
-        "uploaders", condition.select, {
-            "select": field_list
-        }
-    )[0].flat(2);
+    const options = {
+        "table_name": "uploaders",
+        "mapper": generate_mapper(
+            mapping, field_list
+        )
+    };
 
-    const exist_eid = response.select.map(
-        (record) => record.exterior
+    return upsert_item(
+        dataset, options, instance
     );
-
-    const current = instance.toISOString();
-
-    if (exist_eid.length > 0) {
-        const condition = {
-            "column": "exterior",
-            "restrict": {
-                "include": exist_eid
-            }
-        };
-
-        const mapping = {
-            "input": Object.fromEntries(uploaders.map(
-                (uploader) => [ uploader.id, uploader ]
-            )),
-            "database": Object.fromEntries(response.select.map(
-                (record) => [ record.exterior, record ]
-            ))
-        };
-
-        const builder = operator.statement().build;
-
-        const generator = (source, field, hanlder) => {
-            const _handler = builder.case.abstract.single;
-
-            hanlder ??= (item) => item;
-
-            return (options) => {
-                return _handler("exterior", Object.fromEntries(
-                    Object.entries(mapping[source]).map(
-                        ([ eid, uploader ]) => [
-                            eid, hanlder(uploader[field])
-                        ]
-                    )
-                ), options);
-            };
-        };
-
-        const options = {
-            "action": "execute",
-            "return_field": field_list
-        };
-
-        response.update = record.update(
-            "uploaders", condition, {
-                "username": generator("input", "name"),
-                "avatar": generator("input", "avatar"),
-                "update_count": generator("database", "update_count", (item) => item + 1),
-                "updated_at": current
-            }, options
-        )[0].flat(2);
-    }
- 
-    uploaders.insert = uploaders.filter(
-        ({ id }) => !exist_eid.includes(id)
-    );
-
-    const dataset = [];
-
-    for (let index = 0; index < uploaders.insert.length; index++) {
-        const uploader = uploaders.insert[index];
-
-        dataset.push({
-            "exterior": uploader.id,
-            "username": uploader.name,
-            "avatar": uploader.avatar,
-            "update_count": 1,
-            "updated_at": current,
-            "recorded_at": current
-        });
-    }
-
-    if (dataset.length > 0) {
-        const options = {
-            "mode": "batch",
-            "action": "execute",
-            "return_field": field_list
-        };
-        
-        response.insert = record.insert(
-            "uploaders", dataset, options
-        )[0].flat(2);
-    }
-
-    response.insert ??= [];
-    response.update ??= [];
-
-    const { update, insert } = response;
-
-    return [ update, insert ].flat(1);
 }
 
 /**
@@ -200,180 +322,57 @@ function upsert_uploader(uploaders, instance = new Date()) {
  * @returns {Response[]} 更新或者插入的视频信息
  */
 function upsert_video(videos, instance = new Date()) {
-    if (!Array.isArray(videos)) {
-        videos = [ videos ];
-    }
-
-    if (videos.length > MIC) {
-        const responses = [];
-
-        const groups = split_group(videos, MIC);
-
-        for (let index = 0; index < groups.length; index++) {
-            const result = upsert_video(
-                groups[index], instance
-            );
-
-            responses.push(result);
-        }
-
-        return responses.flat(1);
-    }
-
-    const record = operator.record();
-
-    const condition = {};
-    
-    const eid_list = videos.map(
-        (video) => video.id
-    );
-
-    condition.select = {
-        "column": "exterior",
-        "restrict": {
-            "include": eid_list
-        }
+    const format = (instance) => {
+        const text = instance.toISOString();
+        
+        return text.replaceAll(".000", "");
     };
 
-    const response = {}, field_list = [
-        "id", "exterior", "update_count"
+    const dataset = [], mapping = {};
+
+    const vid_to_uid = new Map(
+        upsert_uploader(videos.map(
+            video => video.uploader
+        )).map((result, index) => {
+            const video = videos[index];
+
+            return [ video.id, result.id ];
+        })
+    );
+
+    for (let index = 0; index < videos.length; index++) {
+        const { id, ...rest } = videos[index];
+
+        const data = {
+            "title": rest.title,
+            "uploader_id": vid_to_uid.get(id),
+            "thumbnail": rest.thumbnail,
+            "duration": rest.duration,
+            "page_count": rest.page,
+            "created_at": format(rest.created_at),
+            "published_at": format(rest.published_at),
+            "exterior": id
+        };
+
+        dataset.push(data);
+        mapping[id] = data;
+    }
+
+    const field_list = [
+        "uploader_id",
+        "title", "duration", "page",
+        "page_count", "thumbnail",
+        "created_at", "published_at",
     ];
 
-    response.select = record.select(
-        "videos", condition.select, {
-            "select": field_list
-        }
-    )[0].flat(2);
-
-    /**
-     * @description 已存在的视频外部标识符
-     * 
-     * @type {number[]}
-     */
-    const exist_eid = response.select.map(
-        (record) => record.exterior
-    );
-
-    const current = instance.toISOString();
-
-    const mapping = {
-        "input": Object.fromEntries(videos.map(
-            (video) => [ video.id, video ]
-        )),
-        "related": {  
-            "uploader": Object.fromEntries(upsert_uploader(
-                videos.map((video) => video.uploader), instance
-            ).map(({ id, exterior }) => [ exterior, id ])),
-        },
-        "database": Object.fromEntries(response.select.map(
-            (record) => [ record.exterior, record ]
-        ))
+    const options = {
+        "table_name": "videos",
+        "mapper": generate_mapper(
+            mapping, field_list
+        )
     };
 
-    mapping.uploader = Object.fromEntries(videos.map((video) => {
-        const video_exterior_id = video.id;
-
-        const related = mapping.related.uploader;
-        const uploader_exterior_id = video.uploader.id;
-
-        const object = {
-            "uploader_id": related[uploader_exterior_id]
-        };
-        
-        return [ video_exterior_id, object ];
-    }));
-
-    if (exist_eid.length > 0) {
-        const condition = {
-            "column": "exterior",
-            "restrict": {
-                "include": exist_eid
-            }
-        };
-
-        const builder = operator.statement().build;
-
-        const _handler = builder.case.abstract.single;
-
-        const generator = (source, field, hanlder) => {
-            hanlder ??= (item) => item;
-
-            return (options) => {
-                return _handler("exterior", Object.fromEntries(
-                    Object.entries(mapping[source]).map(
-                        ([ eid, uploader ]) => [
-                            eid, hanlder(uploader[field])
-                        ]
-                    )
-                ), options);
-            };
-        };
-
-        const options = {
-            "action": "execute",
-            "return_field": field_list
-        };
-
-        const handler = {
-            "date": (item) => item.toISOString().replaceAll(".000", ""),
-            "increase": (item) => item + 1
-        };
-
-        response.update = record.update(
-            "videos", condition, {
-                "title": generator("input", "title"),
-                "thumbnail": generator("input", "thumbnail"),
-                "duration": generator("input", "duration"),
-                "page_count": generator("input", "page"),
-                "created_at": generator("input", "created_at", handler.date),
-                "published_at": generator("input", "published_at", handler.date),
-                "uploader_id": generator("uploader", "uploader_id"),
-                "update_count": generator("database", "update_count", handler.increase),
-                "updated_at": current
-            }, options
-        )[0].flat(2);
-    }
- 
-    const inserted = videos.filter(
-        ({ id }) => !exist_eid.includes(id)
+    return upsert_item(
+        dataset, options, instance
     );
-
-    const dataset = [];
-
-    for (let index = 0; index < inserted.length; index++) {
-        const video = inserted[index];
-
-        dataset.push({
-            "exterior": video.id,
-            "title": video.title,
-            "thumbnail": video.thumbnail,
-            "duration": video.duration,
-            "page_count": video.page,
-            "created_at": video.created_at.toISOString().replaceAll(".000", ""),
-            "published_at": video.published_at.toISOString().replaceAll(".000", ""),
-            "uploader_id": mapping.related.uploader[video.uploader.id],
-            "update_count": 1,
-            "updated_at": current,
-            "recorded_at": current
-        });
-    }
-
-    if (dataset.length > 0) {
-        const options = {
-            "mode": "batch",
-            "action": "execute",
-            "return_field": field_list
-        };
-        
-        response.insert = record.insert(
-            "videos", dataset, options
-        )[0].flat(2);
-    }
-
-    response.insert ??= [];
-    response.update ??= [];
-
-    const { update, insert } = response;
-
-    return [ update, insert ].flat(1);
 }
