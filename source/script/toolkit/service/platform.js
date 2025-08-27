bf279375b6c80566dbc27b273af747bdcc047ba0
ETeyondLitle0721 <eteyondlitle@gmail.com>
2025-08-27 13:18:03 +0800

    修改platform数据库架构，函数upsert_uploader已实现

--- source/script/toolkit/service/platform.js
+++ source/script/toolkit/service/platform.js
@@ -18,3 +18,133 @@ const operator = new DatabaseOperator(instance);
 
 const MIC = 8192; // 单次插入标识符最大数量
 
+/**
+ * 更新或者插入上传者信息到数据库中
+ * 
+ * @typedef {Object} Uploader
+ * @property {number} id 上传者外部标识符
+ * @property {string} name 上传者名称
+ * @property {string} avatar 上传者头像
+ * 
+ * @param {(Uploader|Uploader[])} uploaders 需要更新或者插入的上传者信息列表
+ * @param {Date} [instance=new Date()] 执行操作的时候的时间
+ * @returns {void}
+ */
+function upsert_uploader(uploaders, instance = new Date()) {
+    if (!Array.isArray(uploaders)) {
+        uploaders = [ uploaders ];
+    }
+
+    if (uploaders.length > MIC) {
+        const groups = split_group(uploaders, MIC);
+
+        for (const group of groups) {
+            upsert_uploader(group);
+        }
+
+        return;
+    }
+
+    const record = operator.record();
+
+    const condition = {};
+    
+    const eid_list = uploaders.map(
+        (uploader) => uploader.id
+    );
+
+    condition.select = {
+        "column": "exterior",
+        "restrict": {
+            "include": eid_list
+        }
+    };
+
+    const response = {};
+
+    response.select = record.select(
+        "uploaders", condition.select, {
+            "select": [ "exterior", "update_count" ]
+        }
+    )[0].flat(2);
+
+    const exist_eid = response.select.map(
+        (record) => record.exterior
+    );
+
+    const current = instance.toISOString();
+
+    if (exist_eid.length > 0) {
+        const condition = {
+            "column": "exterior",
+            "restrict": {
+                "include": exist_eid
+            }
+        };
+
+        const mapping = {
+            "input": Object.fromEntries(uploaders.map(
+                (uploader) => [ uploader.id, uploader ]
+            )),
+            "database": Object.fromEntries(response.select.map(
+                (record) => [ record.exterior, record ]
+            ))
+        };
+
+        const builder = operator.statement().build;
+
+        const generator = (source, field, hanlder) => {
+            const _handler = builder.case.abstract.single;
+
+            hanlder ??= (item) => item;
+
+            return (options) => {
+                return _handler("exterior", Object.fromEntries(
+                    Object.entries(mapping[source]).map(
+                        ([ eid, uploader ]) => [
+                            eid, hanlder(uploader[field])
+                        ]
+                    )
+                ), options);
+            };
+        };
+
+        response.update = record.update(
+            "uploaders", condition, {
+                "username": generator("input", "name"),
+                "avatar": generator("input", "avatar"),
+                "update_count": generator("database", "update_count", (item) => item + 1),
+                "updated_at": current
+            }
+        );
+    }
+ 
+    uploaders.insert = uploaders.filter(
+        ({ id }) => !exist_eid.includes(id)
+    );
+
+    const dataset = [];
+
+    for (let index = 0; index < uploaders.insert.length; index++) {
+        const uploader = uploaders.insert[index];
+
+        dataset.push({
+            "exterior": uploader.id,
+            "username": uploader.name,
+            "avatar": uploader.avatar,
+            "update_count": 1,
+            "updated_at": current,
+            "recorded_at": current
+        });
+    }
+
+    if (dataset.length > 0) {
+        response.insert = record.insert(
+            "uploaders", dataset, {
+                "mode": "batch"
+            }
+        );
+    }
+
+    return;
+}
